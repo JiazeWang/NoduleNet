cc54ad4b4ee18cf20a9ef32ade9fab4ec40c355c
jiazewang <1155114536@link.cuhk.edu.hk>
2021-01-31 18:23:13 +0800

    test

--- test.py
+++ test.py
@@ -223,6 +223,7 @@ def eval(net, dataset, save_dir=None):
     if not os.path.exists(os.path.join(eval_dir, 'ensemble')):
         os.makedirs(os.path.join(eval_dir, 'ensemble'))
 
+    """
     noduleCADEvaluation('evaluationScript/annotations/LIDC/3_annotation.csv',
     'evaluationScript/annotations/LIDC/3_annotation_excluded.csv',
     dataset.set_name, rpn_submission_path, os.path.join(eval_dir, 'rpn'))
@@ -234,9 +235,38 @@ def eval(net, dataset, save_dir=None):
     noduleCADEvaluation('evaluationScript/annotations/LIDC/3_annotation.csv',
     'evaluationScript/annotations/LIDC/3_annotation_excluded.csv',
     dataset.set_name, ensemble_submission_path, os.path.join(eval_dir, 'ensemble'))
+    """
+    ensemble_submission_path_v1 = os.path.join(eval_dir, 'submission_v1.csv')
+    convert_csv_2_origin(ensemble_submission_path, ensemble_submission_path_v1)
 
+    noduleCADEvaluation('evaluationScript/annotations/LIDC/annotations_origin.csv',
+    'evaluationScript/annotations/LIDC/3_annotation_excluded.csv',
+    dataset.set_name, ensemble_submission_path, os.path.join(eval_dir, 'eval_origin'))
     print
 
+
+def convert_csv_2_origin(filename, outputname):
+    CSV_FILE_PATH = filename
+    df = pd.read_csv(CSV_FILE_PATH)
+    result = df.values.tolist()
+    new = []
+    for i in range(0, len(result)):
+        xyz = np.array([result[i][1], result[i][2], result[i][3]])
+        size = result[i][4]
+        pro = 1
+        spacing = os.path.join("/research/dept8/jzwang/dataset/LUNA16/preprocessed_test", '%s_spacing_origin.npy' % (result[i][0]))
+        origin = os.path.join("/research/dept8/jzwang/dataset/LUNA16/preprocessed_test", '%s_origin.npy' % (result[i][0]))
+        spacing = np.load(spacing)
+        origin = np.load(origin)
+        spacing = np.array(list(reversed(spacing)))
+        origin = np.array(list(reversed(origin)))
+        xyz = xyz*spacing+origin
+        new.append([result[i][0], xyz[0], xyz[1], xyz[2], size, pro])
+    #new = np.concatenate(new, axis=0)
+    col_names = ['seriesuid','coordX','coordY','coordZ','diameter_mm', 'probability']
+    submission_path = outputname
+    df = pd.DataFrame(new, columns=col_names)
+    df.to_csv(submission_path, index=False)
 
 def eval_single(net, input):
     with torch.no_grad():
