348f330f3312fd5a98d2bc4b2f026234dd9ad030
jiazewang <115511436@link.cuhk.edu.hk>
2021-02-02 02:43:19 +0800

    update

--- dlung_v1.1/config_lidc.py
+++ dlung_v1.1/config_lidc.py
@@ -12,7 +12,7 @@ torch.manual_seed(SEED)
 torch.cuda.manual_seed_all(SEED)
 
 # Preprocessing using preserved HU in dilated part of mask
-BASE = '/research/dept8/jzwang/code/NoduleNet/dlung_v1/data/' # make sure you have the ending '/'
+BASE = BASE = os.path.realpath(__file__)[:-14] # make sure you have the ending '/'
 data_config = {
     'data_dir': BASE,
     'data_txt': BASE + 'filedir.txt',
--- dlung_v1.1/readme.txt
+++ dlung_v1.1/readme.txt
@@ -15,11 +15,13 @@ cd build/box
 python setup.py install
 
 3 Preprocess data:
+put the datafolder under data like CT-Lung
 cd data
 python filedir.py
 cd utils
 python preprocess_lidc.py
 
 4 Run inference:
+cd ..
 python test_lung.py eval
 Then we can get the result in the json: result.json
