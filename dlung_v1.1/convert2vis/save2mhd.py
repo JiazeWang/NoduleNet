96b4534b18b2ed782fad9a81044034f2a2ccc8e5
jiazewang <115511436@link.cuhk.edu.hk>
2021-02-04 11:30:15 +0800

    save2mhd

---
+++ dlung_v1.1/convert2vis/save2mhd.py
@@ -0,0 +1,34 @@
+import SimpleITK as sitk
+import numpy as np
+from save_sitk import savedicom
+
+def load_itk_image(filename):
+    """Return img array and [z,y,x]-ordered origin and spacing
+    """
+
+    itkimage = sitk.ReadImage(filename)
+    numpyImage = sitk.GetArrayFromImage(itkimage)
+
+    numpyOrigin = np.array(list(reversed(itkimage.GetOrigin())))
+    numpySpacing = np.array(list(reversed(itkimage.GetSpacing())))
+
+    return numpyImage, numpyOrigin, numpySpacing
+
+
+
+def main():
+    filelist = "/research/dept8/jzwang/code/lung_nodule_detector/dlung_v1/data/patient_mhd.txt"
+    with open(filelist, "r") as f:
+        lines = f.readlines()
+    params_lists = []
+    for line in lines:
+        line = line.rstrip()
+        filefullname = "/research/dept8/jzwang/code/lung_nodule_detector/dlung_v1/data/mhd/"+line
+        input _, _ = load_itk_image(filefullname)
+        outputdir="result/"+line
+        if not os.path.exists(outputdir):
+            os.makedirs(outputdir)
+        savedicom(outputdir, input,  pixel_dtypes="int16")
+
+if __name__=='__main__':
+    main()
